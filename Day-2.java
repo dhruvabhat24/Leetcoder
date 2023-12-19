1f719e4f8b3f7486e9f747ef3882b841879bb581
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-12-19 21:30:28 +0530

    Create Day-2.java

---
+++ Day-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int gcd = gcd(len1, len2);
+        return str1.substring(0, gcd);
+    }
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
