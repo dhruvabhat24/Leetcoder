40590be838ac2e30a3028acd607adae4bf04b9ed
Abinand P <abinand0911@gmail.com>
2024-01-06 22:06:37 +0530

    Adding programs

---
+++ Daily Coding Challenge/Maximum Profit in job scheduling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int numJobs = profit.size(); // Number of jobs
+        vector<tuple<int, int, int>> jobs(numJobs); // vector of tuple which stores the value of the startTime, endTime, profit of the same entity
+      
+        for (int i = 0; i < numJobs; ++i) {
+            jobs[i] = {endTime[i], startTime[i], profit[i]};
+        }
+      
+        sort(jobs.begin(), jobs.end());
+        vector<int> dp(numJobs + 1); //Creating a dp 
+      
+        for (int i = 0; i < numJobs; ++i) {
+            auto [endTime, startTime, profit] = jobs[i];
+          
+            int latestNonConflictJobIndex = upper_bound(jobs.begin(), jobs.begin() + i, startTime, [&](int time, const auto& job) -> bool {
+                return time < get<0>(job);
+            }) - jobs.begin();
+          
+            dp[i + 1] = max(dp[i], dp[latestNonConflictJobIndex] + profit);
+        }
+      
+        return dp[numJobs];
+    }
+};
